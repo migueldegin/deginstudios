be1ac9da3049e8d13afc6b8e83a4ecc7ad0af32d
Miguel <deginh@gmail.com>
2026-06-26 17:25:58 +0200

    Domain fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://migueldegin.github.io',
-  base: '/deginstudios',
-  output: 'static',
+  site: 'https://deginstudios.com',
 });
